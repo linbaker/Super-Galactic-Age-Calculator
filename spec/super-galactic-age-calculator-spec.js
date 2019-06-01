e13f2aeff58774250da265f8260b84287120c384
Lindsey <lindsey.baker0@gmail.com>
2019-05-31 17:05:40 -0700

    Add jasmine support files and packages

--- spec/super-galactic-age-calculator-spec.js
+++ spec/super-galactic-age-calculator-spec.js
@@ -2,7 +2,7 @@ import { age } from './../src/super-galactic-age-calculator.js';
 
 describe('age', function() {
   it('should return years between current time and user inputted date of birth', function() {
-    let dob = new Date(1993, 08, 11);
-    expect(dob.age().toEqual(25));
+    let dob = new Date(1993, 8, 11);
+    expect(age(dob)).toEqual(25);
   });
 });
--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
-import { Age } from './super-galactic-age-calculator.js';
+// import { Age } from './super-galactic-age-calculator.js';
 import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-var age = new Age();
 $(document).ready(function(){
-  console.log(age);
   });
