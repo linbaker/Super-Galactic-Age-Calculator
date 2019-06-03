b59c6f221df49bfe1fe26b29e75795c4325cf6c7
Lindsey <lindsey.baker0@gmail.com>
2019-06-02 17:50:21 -0700

    Add test for ageOnVenus function

--- spec/super-galactic-age-calculator-spec.js
+++ spec/super-galactic-age-calculator-spec.js
@@ -16,7 +16,13 @@ describe('GalacticAgeCalculator', function() {
   it('should return Mercury solar years between current time and user inputted date of birth', function() {
     let dob = new GalacticAgeCalculator(new Date(1993, 8, 11));
     expect(dob.ageOnMercury()).toEqual(104);
-    expect(dob.ageOnEarth()).not.toEqual(20);
+    expect(dob.ageOnMercury()).not.toEqual(20);
+  });
+
+  it('should return Venus solar years between current time and user inputted date of birth', function() {
+    let dob = new GalacticAgeCalculator(new Date(1993, 8, 11));
+    expect(dob.ageOnVenus()).toEqual(40);
+    expect(dob.ageOnVenus()).not.toEqual(20);
   });
 
 });
