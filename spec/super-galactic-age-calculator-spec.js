2850d11534b2cbde5a8114269485815d9db78a77
Lindsey <lindsey.baker0@gmail.com>
2019-06-02 17:18:13 -0700

    Add additional spec to age on earth test

--- spec/super-galactic-age-calculator-spec.js
+++ spec/super-galactic-age-calculator-spec.js
@@ -8,8 +8,9 @@ describe('GalacticAgeCalculator', function() {
   });
 
   it('should return years between current time and user inputted date of birth', function() {
-    let dob = new GalacticAgeCalculator(new Date("1993, 8, 11"));
-    expect(dob.age()).toEqual(25);
+    let dob = new GalacticAgeCalculator(new Date(1993, 8, 11));
+    expect(dob.ageOnEarth()).toEqual(25);
+    expect(dob.ageOnEarth()).not.toEqual(20);
   });
 
 });
