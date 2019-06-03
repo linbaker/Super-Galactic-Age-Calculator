f25f9a09fba3154181610db5a3120900b31d925e
Lindsey <lindsey.baker0@gmail.com>
2019-06-02 19:38:27 -0700

    Add life expectancy on Venus test

--- spec/super-galactic-age-calculator-spec.js
+++ spec/super-galactic-age-calculator-spec.js
@@ -50,4 +50,12 @@ describe('GalacticAgeCalculator', function() {
     expect(dob.yearsLeftMercury()).not.toEqual("You are 109 years old on Mercury. You have 1 Mercury solar years left before you kick the bucket!");
     expect(dob2.yearsLeftMercury()).toEqual("You are 412 years old on Mercury. You have over stayed your welcome on Mercury by 117 Mercury solar years.");
   });
+
+  it('should return life expectancy on Venus', function() {
+    let dob = new GalacticAgeCalculator(new Date(1993, 8, 11));
+    let dob2 = new GalacticAgeCalculator(new Date(1920, 1, 1));
+    expect(dob.yearsLeftVenus()).toEqual("You are 40 years old on Venus. You have 74 Venus solar years left before you kick the bucket!");
+    expect(dob.yearsLeftVenus()).not.toEqual("You are 109 years old on Venus. You have 1 Venus solar years left before you kick the bucket!");
+    expect(dob2.yearsLeftVenus()).toEqual("You are 159 years old on Venus. You have over stayed your welcome on Venus by 46 Venus solar years.");
+  });
 });
